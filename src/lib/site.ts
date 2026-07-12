855163df89a80a242431880ffd959e059e8cefd4
Yike_Lin <3100281535@qq.com>
2026-07-12 21:02:06 +0800

    添加 navigation.ts 文件以定义网站导航链接

---
+++ src/lib/site.ts
@@ -0,0 +1,6 @@
+export const navigation = [
+  { href: "/recipes", label: "配方" },
+  { href: "/cabinet", label: "酒柜" },
+  { href: "/mix", label: "调制台" },
+  { href: "/taste/demo-negroni", label: "品鉴" }
+];
